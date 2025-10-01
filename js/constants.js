27742eb6ccdd93f1bcdf5a7ad41804b405339756
Andrew Rath <arath5@gmail.com>
2025-10-01 10:13:34 -0700

    Update wind resistance calculations

--- js/constants.js
+++ js/constants.js
@@ -5,11 +5,12 @@ class Constants {
     bikeCrossSectionalArea = this.bikeWheelRadius * 2 * this.bikeWheelThickness; //(I am going to assume in m^2. I don't know what units they used for the current implementation)
     airDensity = 1.225; // kg/m^3
     airTemperature = 303; //in Kelvin
-    airViscosity = ((1.458 * 10 ^ -6 * this.airTemperature ^ (3 / 2)) / (this.airTemperature + 110.4)) / this.airDensity; //m^2/s
+    airViscosity = ((1.458 * Math.pow(10, -6) * Math.pow(this.airTemperature, 3 / 2)) / (this.airTemperature + 110.4)) / this.airDensity; //m^2/s
     windResistance(velocity) { //m/s
-        let equivalentDiameter = 1.3 * ((this.bikeWheelRadius * 2 * this.bikeWheelThickness) ^ 0.625) / ((this.bikeWheelRadius * 2 + this.bikeWheelThickness) ^ 0.25);
-        let reynoldsNumber = (equivalentDiameter * velocity) / this.airViscosity;
-        let dragCoefficient = reynoldsNumber
+        let equivalentDiameter = 1.3 * (Math.pow(this.bikeWheelRadius * 2 * this.bikeWheelThickness, 0.625)) / (Math.pow(this.bikeWheelRadius * 2 + this.bikeWheelThickness, 0.25));
+        //let reynoldsNumber = (equivalentDiameter * velocity) / this.airViscosity;
+        let shearStress = this.airViscosity / this.bikeCrossSectionalArea;
+        let dragCoefficient = (shearStress) / (this.airDensity * velocity * velocity);
         return 0.5 * this.airDensity * velocity * velocity * this.bikeCrossSectionalArea * dragCoefficient;
     };
 }
