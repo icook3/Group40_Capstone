d0caa703d0f36634e44ef8f517abdb6dbe359759
egdunham <Elizabeth.Dunham@asu.edu>
2025-10-03 00:39:20 -0700

    Preliminary move cloud generation from index.html to scene.js

--- js/scene.js
+++ js/scene.js
@@ -8,6 +8,7 @@ export class ZlowScene {
     this.objects = [];
     this.objectsLoaded = false;
     this.edgeObjects = [];
+
     // Helper functions for DRY object creation
     this._createBuilding = (x, y, z, w, h, d) => {
       const obj = document.createElement('a-entity');
@@ -17,6 +18,7 @@ export class ZlowScene {
       obj.setAttribute('position', `${x} ${y} ${z}`);
       return obj;
     };
+
     this._createTree = (x, z) => {
       const obj = document.createElement('a-entity');
       const trunk = document.createElement('a-entity');
@@ -32,7 +34,18 @@ export class ZlowScene {
       obj.setAttribute('position', `${x} 0 ${z}`);
       return obj;
     };
+
+    this._createCloud = (radius, opacity, x, y, z) => {
+      const cloud = document.createElement('a-entity');
+      cloud.setAttribute('geometry', `primitive: sphere; radius: ${radius}`);
+      cloud.setAttribute('material', `color: #fff; opacity: ${opacity}; transparent: true`);
+      cloud.setAttribute('position', `${x} ${y} ${z}`);
+
+      //<a-entity geometry="primitive: sphere; radius: 7" material="color: #fff; opacity: 0.7; transparent: true" position="-40 22 -120"></a-entity>
+      return cloud;
+    };
     this._initEdgeObjects();
+    //this._initClouds();
   }
 
   _initEdgeObjects() {
@@ -66,6 +79,44 @@ export class ZlowScene {
       this._spawnObject(z);
       // Optionally spawn a second object at the same z for more density
       if (Math.random() < 0.7) this._spawnObject(z);
+    }
+  }
+
+  // Initiate clouds. Measurement array hardcoded pending further development
+
+  _initClouds() {
+    // Radius, opacity, position
+    const cloud_dimensions = [
+      [7,0.7,-40,22,-120],
+      [5,0.6,-32,23,-110],
+      [6,0.65,-36,21,-130],
+      [8,0.7,35,24,-140],
+      [6,0.6,42,23,-150],
+      [5,0.5,38,25,-135],
+      [4,0.5,0,28,-160],
+      [6,0.6,-10,26,-170],
+      [18,0.55,-70,22,-60],
+      [12,0.5,-85,25,-75],
+      [10,0.45,-60,20,-45],
+      [17,0.55,70,23,-65],
+      [13,0.5,85,26,-80],
+      [11,0.45,60,21,-50],
+      [10,0.35,-25,38,-70],
+      [8,0.32,25,40,-80],
+      [12,0.3,0,42,-90],
+      [7,0.28,-50,36,-100],
+      [9,0.3,50,37,-110],
+      [18,0.18,-60,55,-180],
+      [15,0.15,60,58,-200],
+      [20,0.12,0,60,-220],
+      [13,0.14,-80,53,-160],
+      [11,0.13,80,54,-170]
+    ]
+
+    for (let i = 0; i < cloud_dimensions.length; i++) {
+      let cloud = this._createCloud(cloud_dimensions[i][0], cloud_dimensions[i][1], cloud_dimensions[i][2], cloud_dimensions[i][3], cloud_dimensions[i][4]);
+      this.scene.appendChild(cloud);
+      this.objects.push(cloud);
     }
   }
 
