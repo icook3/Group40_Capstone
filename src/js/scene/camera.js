ea0db14bb7300169c33468eb92af890832d6410d
legowiifun <arath5@gmail.com>
2025-11-05 10:42:14 -0700

    Rework camera.js into a singleton

--- src/js/scene/camera.js
+++ src/js/scene/camera.js
@@ -1,50 +1,52 @@
-let camera = document.getElementById("camera");
-let rig = document.getElementById("rig");
-/**
- * Set the position of the camera
- * @param {x, y, z} pos 
- */
-export function setCameraPosition(pos) {
-    rig.setAttribute('position', `${pos.x} ${pos.y} ${pos.z}`);
-}
-/**
- * Set the rotation of the camera
- * @param {x, y, z} rot 
- */
-export function setCameraRotation(rot) {
-    rig.setAttribute('rotation', `${rot.x} ${rot.y} ${rot.z}`);
-}
+class Camera {
+    camera = document.getElementById("camera");
+    rig = document.getElementById("rig");
+    /**
+    * Set the position of the camera
+    * @param {x, y, z} pos 
+    */
+    setCameraPosition(pos) {
+        rig.setAttribute('position', `${pos.x} ${pos.y} ${pos.z}`);
+    }
+    /**
+    * Set the rotation of the camera
+    * @param {x, y, z} rot 
+    */
+    setCameraRotation(rot) {
+        rig.setAttribute('rotation', `${rot.x} ${rot.y} ${rot.z}`);
+    }
 
-export function zoomCamera(zoom) {
-    camera.setAttribute("zoom", zoom);
-}
-export function setFoV(fov) {
-    camera.setAttribute("fov",fov);
-}
+    zoomCamera(zoom) {
+        camera.setAttribute("zoom", zoom);
+    }
+    setFoV(fov) {
+        camera.setAttribute("fov",fov);
+    }
 
-export function getFoV() {
-    return camera.getAttribute("fov");
-}
+    getFoV() {
+        return camera.getAttribute("fov");
+    }
 
-export function getZoom() {
-    return camera.getAttribute("zoom");
-}
+    getZoom() {
+        return camera.getAttribute("zoom");
+    }
 
-export function getCameraPosition() {
-  let val = rig.getAttribute('position');
-  if (typeof val === 'string') val = AFRAME.utils.coordinates.parse(val);
-  return { x: +val.x || 0, y: +val.y || 0, z: +val.z || 0 };
-}
+    getCameraPosition() {
+        let val = rig.getAttribute('position');
+        if (typeof val === 'string') val = AFRAME.utils.coordinates.parse(val);
+        return { x: +val.x || 0, y: +val.y || 0, z: +val.z || 0 };
+    }
+    getCameraRotation() {
+        let val = rig.getAttribute('rotation');
+        if (typeof val === 'string') val = AFRAME.utils.coordinates.parse(val);
+        return { x: +val.x || 0, y: +val.y || 0, z: +val.z || 0 };
+    }
 
-export function getCameraRotation() {
-  let val = rig.getAttribute('rotation');
-  if (typeof val === 'string') val = AFRAME.utils.coordinates.parse(val);
-  return { x: +val.x || 0, y: +val.y || 0, z: +val.z || 0 };
+    defaultCamera() {
+        setCameraRotation({x: 0, y: 0, z: 0});
+        setCameraPosition({x: 0, y: 4.5, z: 5});
+        zoomCamera(1);
+        setFoV(80);
+    }
 }
-
-export function defaultCamera() {
-    setCameraRotation({x: 0, y: 0, z: 0});
-    setCameraPosition({x: 0, y: 4.5, z: 5});
-    zoomCamera(1);
-    setFoV(80);
-}
+export let camera = new Camera();
