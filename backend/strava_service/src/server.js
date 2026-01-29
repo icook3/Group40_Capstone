391d14c95018ef32037b14ba2122519e96eb1bfa
Isaac Cook <icook3@asu.edu>
2026-01-28 17:56:00 -0800

    Update log message for Strava service

--- backend/strava_service/src/server.js
+++ backend/strava_service/src/server.js
@@ -4,5 +4,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-    console.log(`Strava OAuth Service is running on port ${PORT}`);
+    console.log(`Strava Service is running on port ${PORT}`);
 });
