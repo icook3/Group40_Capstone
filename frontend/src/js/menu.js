7098ea3cc8da940fc932c23805c3a4720b01e056
legowiifun <arath5@gmail.com>
2026-02-06 10:07:22 -0700

    Set the setting in the local storage when a button is pressed on the main menu

--- frontend/src/js/menu.js
+++ frontend/src/js/menu.js
@@ -121,6 +121,27 @@ export function initSettings() {
       });
   }
   */
+
+  // clear workout storage
+  const workoutStorageBtn = document.getElementById("workoutStorageSetting");
+  if (workoutStorageBtn) {
+      workoutStorageBtn.addEventListener("click", () => {
+          localStorage.setItem("autoClearWorkoutStorage", false);
+          let clearStorage = sessionStorage.getItem("autoClearWorkoutStorage");
+      if (clearStorage==null) {
+        clearStorage = false;
+      } else if (clearStorage == "false") {
+        clearStorage = false;
+      } else {
+        clearStorage = true;
+      }
+      clearStorage = !clearStorage;
+      sessionStorage.setItem("autoClearWorkoutStorage",clearStorage);
+      workoutStorageBtn.textContent = clearStorage
+        ? "Automatically clear workout storage: ON"
+        : "Automatically clear workout storage";
+      });
+  }
 }
 
 async function initStravaButton() {
