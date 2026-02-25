b97473164806548f1ecc0fea63a1e2bc0b62e2b8
legowiifun <arath5@gmail.com>
2026-02-25 11:09:55 -0700

    Clean up some console logs that are no longer needed I hope they are no longer needed.

--- frontend/src/js/views/viewManager.js
+++ frontend/src/js/views/viewManager.js
@@ -55,7 +55,7 @@ export class ViewManager {
     }
 
     setView(view, usingBrowser=false) {
-        console.group();
+        //console.group();
         console.log("SETTING VIEW TO "+view);
         //reset the page you are currently on
         switch(this.currentView) {
@@ -104,23 +104,23 @@ export class ViewManager {
                 break;
             default: 
                 console.log("This view is not available! "+view);
-                console.groupEnd();
+                //console.groupEnd();
                 return;
                 break;
         }
         
         // if you are not using browser buttons, add it to the stack
         if (!usingBrowser) {
-            console.log("Adding "+this.currentView+" to the stack");
+            //console.log("Adding "+this.currentView+" to the stack");
             //push the current view onto the stack
             this.pastScreens.push(this.currentView);
-            console.log("stack contains: ",this.pastScreens);
+            //console.log("stack contains: ",this.pastScreens);
             this.hashChangeThroughCode=true;
             this.historyIndex++;
             
             //history.pushState({},"");
         }
-        console.groupEnd();
+        //console.groupEnd();
 
         window.location.hash=view;
         if (!usingBrowser) {
@@ -143,46 +143,51 @@ export class ViewManager {
         favicon.href = "../../resources/favicons/ZlowFavicon.svg";
       }
     }
+
     newPopStateFunction(owner, event) {
-        console.group();
+        //console.group();
         
-        console.log("past screens=",owner.pastScreens,"Future screens=",owner.futureScreens);
-        console.log("hashChangeThroughCode=",owner.hashChangeThroughCode);
-        console.log("history state=",owner.lastState);
+        //console.log("past screens=",owner.pastScreens,"Future screens=",owner.futureScreens);
+        //console.log("hashChangeThroughCode=",owner.hashChangeThroughCode);
+        //console.log("history state=",owner.lastState);
         //if you are not actually hitting the back button, but instead are changing the hash through code
         if (owner.hashChangeThroughCode==true) {
             event.preventDefault();
-            console.groupEnd();
+            //console.groupEnd();
             owner.futureScreens=[];
             owner.hashChangeThroughCode=false;
             return;
         }
-     
+        if (history.state==null) {
+            event.preventDefault();
+            //console.groupEnd();
+            return;
+        }
         let currentState = window.history.state;
-        console.log("current state=",currentState);
+        //console.log("current state=",currentState);
         //if you are going backwards
         if (currentState.idx<=this.lastState.idx) {
             //if you are going to a past screen
             if (owner.pastScreens.length==0) {
                 //owner.formerPopStateFunction();
-                console.groupEnd();
+                //console.groupEnd();
                 return;
             }
             console.log("click back button");
             owner.futureScreens.push(owner.currentView);
             owner.setView(owner.pastScreens.pop(), true);
-            console.log("AFTER: past screens=",owner.pastScreens, "Future screens=",owner.futureScreens);
-            console.groupEnd();
+            //console.log("AFTER: past screens=",owner.pastScreens, "Future screens=",owner.futureScreens);
+            //console.groupEnd();
         } else if (currentState.idx>owner.lastState.idx) {
             if (owner.futureScreens.length==0) {
-                console.groupEnd();
+                //console.groupEnd();
                 return;
             }
             console.log("click forwards button");
             owner.pastScreens.push(owner.currentView);
             owner.setView(owner.futureScreens.pop(), true);
-            console.log("AFTER: past screens=",owner.pastScreens, "Future screens=",owner.futureScreens);
-            console.groupEnd();
+            //console.log("AFTER: past screens=",owner.pastScreens, "Future screens=",owner.futureScreens);
+            //console.groupEnd();
         }
         owner.lastState=currentState;
     }
