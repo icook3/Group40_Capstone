1521504696d9a624fb6dc28a61d5cdde5a8ce5ab
racorder <128233662+racorder@users.noreply.github.com>
2026-04-02 22:46:16 -0500

    Stop leg and pedal animation when power is 0

--- frontend/src/js/avatarMovement.js
+++ frontend/src/js/avatarMovement.js
@@ -95,7 +95,8 @@ export class AvatarMovement {
 
         //Rotate crank and pedals
         if (this.creator.leftPedalBone && this.creator.rightPedalBone && this.creator.pedalCrankBone) {
-            if (this.creator.id === "pacer-entity") {
+            if (this.power !== 0) {
+                if (this.creator.id === "pacer-entity") {
                 //Rotate crank
                 const pacerCrankAngularSpeed = ((this.speed * angularSpeedAdjuster / baseSpeed * 1000 / 3600)) / 0.16;
                 const pacerCrankRotationAmount = pacerCrankAngularSpeed * dt;
@@ -104,16 +105,18 @@ export class AvatarMovement {
                 //Rotate pedals
                 this.creator.leftPedalBone.rotation.y = this.creator.pedalCrankBone.rotation.x;
                 this.creator.rightPedalBone.rotation.y = -this.creator.pedalCrankBone.rotation.x;
-            } else {
-                //Rotate crank
-                const crankAngularSpeed = ((this.speed * angularSpeedAdjuster / baseSpeed * 1000 / 3600)) / 0.16;
-                const crankRotationAmount = crankAngularSpeed * dt;
-                this.creator.pedalCrankBone.rotation.x -= crankRotationAmount;
+                } else {
+                    //Rotate crank
+                    const crankAngularSpeed = ((this.speed * angularSpeedAdjuster / baseSpeed * 1000 / 3600)) / 0.16;
+                    const crankRotationAmount = crankAngularSpeed * dt;
+                    this.creator.pedalCrankBone.rotation.x -= crankRotationAmount;
 
-                //Rotate pedals
-                this.creator.leftPedalBone.rotation.y = this.creator.pedalCrankBone.rotation.x;
-                this.creator.rightPedalBone.rotation.y = -this.creator.pedalCrankBone.rotation.x;
+                    //Rotate pedals
+                    this.creator.leftPedalBone.rotation.y = this.creator.pedalCrankBone.rotation.x;
+                    this.creator.rightPedalBone.rotation.y = -this.creator.pedalCrankBone.rotation.x;
+                }
             }
+            
         }
     }
 
@@ -154,6 +157,9 @@ export class AvatarMovement {
         }
         if (this.creator.leftPedalBone!=null) {
             this.animatePedalingBike(dt);
+            if (this.power !== 0) {
+                this.animatePedalingPerson(dt);
+            }
             this.animatePedalingPerson(dt);
         }
     }
