c0842e5bacec5f0d243e45105d8b8e302be0abd4
legowiifun <arath5@gmail.com>
2026-04-13 11:42:19 -0700

    Add a way to store terrains in localStorage

--- frontend/src/js/scene/terrains/terrainSwitcher.js
+++ frontend/src/js/scene/terrains/terrainSwitcher.js
@@ -2,20 +2,29 @@ import { DefaultTerrain } from "./DefaultTerrain.js";
 import { TestTerrain } from "./TestTerrain.js";
 class TerrainSwitcher {
     terrains = {
-        defaultTerrain: new DefaultTerrain(),
-        testTerrain: new TestTerrain()
+        defaultTerrain: {terrain: new DefaultTerrain(), name: "defaultTerrain"},
+        testTerrain: {terrain: new TestTerrain(), name: "testTerrain"}
     };
     /**
      * @type {DefaultTerrain}
      */
     currentTerrain=this.terrains.defaultTerrain;
     /**
-     * Can technically be any terrain
-     * Use the terrains object to get all possible terrains
-     * @param {DefaultTerrain} terrain 
+     * Pass in objects from the terrains object
+     * @param {{terrain: DefaultTerrain, name: string}} terrain 
      */
     setTerrain(terrain) {
-        this.currentTerrain=terrain;
+        this.currentTerrain=terrain.terrain;
+        localStorage.setItem("Terrain",terrain.name);
+    }
+    constructor() {
+        let startTerrain = localStorage.getItem("Terrain");
+        if (startTerrain==null) {
+            this.setTerrain(this.terrains.defaultTerrain);
+        } else {
+            //use this syntax to avoid a lengthy switch statement
+            this.currentTerrain=this.terrains[startTerrain].terrain;
+        }
     }
 }
 
