8d5cd18d659264d05234608f455257ee4d102448
legowiifun <arath5@gmail.com>
2026-04-20 10:40:12 -0700

    Create a test for building crash reports

--- tests/crashes.test.js
+++ tests/crashes.test.js
@@ -1,4 +1,8 @@
 import { describe, expect, test } from '@jest/globals';
+
+//focus of the tests
+import {buildCrashReport} from '../backend/crash_logging_service/src/models/crashModel.js';
+
 /**
  * You must run the crashlog server first using Docker
  * Set this to the expected URL - it is currently set at the defaults
@@ -12,5 +16,22 @@ describe('Crashlog storage backend tests', () => {
             //console.log(value);
             expect(value).toBe('ok');
         });
+    });
+    test('builds crash report correctly',()=> {
+        let report = buildCrashReport({errorMessage: "ERROR",stackTrace:"abc.js line 2\ncde.js line 3",otherData1: "DATADATA",otherData2: "OTHERDATA"});
+        let idx = report.id.indexOf('_');
+        expect(idx).not.toBe(-1);
+        //test that the id date part is within a certain range
+        expect(Number(report.id.substring(0,idx))).toBeGreaterThanOrEqual(Date.now()-5);
+        expect(Number(report.id.substring(0,idx))).toBeLessThanOrEqual(Date.now()+5);
+        //check that the ID ends are different every time
+        let idEnd=report.id.substring(idx+1);
+        expect(idEnd.length).toBe(6);
+        let report2 = buildCrashReport({errorMessage: "ERROR2",stackTrace:"abc.js line 1\ncde.js line 5",otherData1: "DATADATA",otherData2: "OTHERDATA"});
+        idx = report2.id.indexOf('_');
+        expect(idEnd).not.toBe(report2.id.substring(idx+1));
+        //check that metadata is filled correctly
+        expect(report.metadata.otherData1).toBe("DATADATA");
+        expect(report.metadata.otherData2).toBe("OTHERDATA");
     });
 });
