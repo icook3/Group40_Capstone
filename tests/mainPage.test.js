a4a1d217de43b2c9777ee698453168e07ea80c9f
legowiifun <arath5@gmail.com>
2026-04-24 11:26:00 -0700

    Create some selenium tests for the keyboard movement. I think this can classify under HUD updating.

--- tests/mainPage.test.js
+++ tests/mainPage.test.js
@@ -1,10 +1,21 @@
 import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
 
-(async function mainPage() {
+(async function testStart() {
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         await driver.get("http://localhost:8000/src/html/index.html");
+        driver.executeScript('localStorage.setItem("testMode","True");');
         await driver.findElement(By.id("start-btn")).click();
+        await driver.sleep(2000);
+        let workoutDialog=await driver.findElement(By.className("workout-dialog"));
+        if (!(await workoutDialog.getText()).startsWith("Free Ride starts in")) {
+            throw new Error("Can not find workout start dialog!");
+        }
+        await driver.findElement(By.css("body")).sendKeys("qqqqq");
+        await driver.sleep(2000);
+        if (!(await driver.findElement(By.id("power")).getText())=="50") {
+            throw new Error("Power is not set correctly");
+        }
     } catch(e) {
         console.log("ERROR!",e);
     } finally {
