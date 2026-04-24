f03a282c11ae6d538daa0094e19ace04ee2e018c
legowiifun <arath5@gmail.com>
2026-04-24 11:48:03 -0700

    Expand the automated selenium tests

--- tests/mainPage.test.js
+++ tests/mainPage.test.js
@@ -1,6 +1,6 @@
 import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
 
-(async function testStart() {
+(async function automatedTests() {
     const driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
         await driver.get("http://localhost:8000/src/html/index.html");
@@ -15,6 +15,19 @@ import {Builder, Browser, By, Key, until} from 'selenium-webdriver';
         await driver.sleep(2000);
         if (!(await driver.findElement(By.id("power")).getText())=="50") {
             throw new Error("Power is not set correctly");
+        }
+        await driver.findElement(By.id("menu-btn")).click();
+        await driver.wait(until.alertIsPresent(),5000);
+        let alert=await driver.switchTo().alert();
+        await alert.accept();
+        await driver.wait(until.alertIsPresent(),5000);
+        alert=await driver.switchTo().alert();
+        await alert.accept();
+        await driver.sleep(2000);
+        await driver.findElement(By.id("customize-btn")).click();
+        await driver.sleep(2000);
+        if (!await driver.findElement(By.id("customize-title")).getText()=="Character Customization") {
+            throw new Error("Character Customization text is wrong. We are on the wrong screen. ")
         }
     } catch(e) {
         console.log("ERROR!",e);
