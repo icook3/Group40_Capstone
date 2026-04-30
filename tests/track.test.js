5c0f693ecb4d1561acdd08d0c1c7e38449ba4e38
Elizabeth Dunham <Elizabeth.Dunham@asu.edu>
2026-04-30 16:42:12 -0700

    Update track.test.js

--- tests/track.test.js
+++ tests/track.test.js
@@ -1,11 +1,38 @@
 /**
  * @jest-environment jsdom
  */
+//IMPORTING config, to prevent bugs
+import * as config from '../frontend/src/js/config/config.js';
+import { constants } from "../frontend/src/js/constants.js";
+import { describe, expect, test } from '@jest/globals';
+import { Track } from "../frontend/src/js/scene/env/Track.js"
 
 const THREE = global.THREE = require('three');
-import { __getRider} from "../frontend/src/js/scene/env/Track.js"
+const scene = new THREE.Scene();
+const track = new Track({ scene: scene });
 
-test('getRider functionality', () => {
-  //expect(sum(1, 2))
-  expect(__getRider()).toBeInstanceOf(THREE.Group());
+describe('track generation and properties', () => {
+
+  test('fundamental track properties', () => {
+    const MeshStandardMaterial = THREE.MeshStandardMaterial;
+    expect(track.path_element.name).toBe("track");
+    expect(track.path_element.type).toBe("Group");
+    expect(track._ownsPath).toBe(true);
+    expect(track.trackMaterial.isMeshStandardMaterial);
+    expect(track.trackMaterialDouble.isMeshStandardMaterial);
+  });
+
+  test('at least 240 track pieces should be generated to start', () => {
+    // track test points should be at least 80 things long
+    expect(constants.trackPoints.length).toBeGreaterThan(240);
+  });
+
+  test('track should include both curved and straight pieces', () => {
+    expect(constants.trackPoints).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({length: 5}),
+        expect.objectContaining({length: 16.55})
+      ])
+    );
+  });
 });
